37571bb50981a9cee1def4c89462337d02b820ec
Remy Arthur <remy.arthur@tuwien.ac.at>
2022-09-07 16:09:07 +0200

    Add the possibility to log Timetable variable

--- writeToScreenAndFile.m
+++ writeToScreenAndFile.m
@@ -1,6 +1,16 @@
+%% Log data in a file and trace in Matlab
+% Param
+%   data : string or timetable : Data to print
+%
+% Return nothing
+
 function writeToScreenAndFile(data)
     % Write data in log file
-    writeDataInFile('log.txt', data);
+    if ( class(data) == "timetable" )
+        writetimetable(data, "log.txt", WriteMode='append', Delimiter='tab');
+    else
+        writeDataInFile('log.txt', data);
+    end
 
     % Display data in "Command window"
     disp(data);
