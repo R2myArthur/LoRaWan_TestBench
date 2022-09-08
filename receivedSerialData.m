145f37688f1b8979b9cf3806ff366519f214d98b
Remy Arthur <remy.arthur@tuwien.ac.at>
2022-09-08 16:48:15 +0200

    Clean up Project for running with RAK2287 and device STM L072Z

--- receivedSerialData.m
+++
@@ -1,8 +0,0 @@
-function receivedSerialData(src,evt)
-    data_received = readline(src);
-    src.UserData(end+1) = data_received;
-%     write(asyncBuff, data_received);
-%     disp(data_received);
-    disp(strcat("Data received: ", data_received));
-    writeDataInFile("test.txt", data_received);
-end
